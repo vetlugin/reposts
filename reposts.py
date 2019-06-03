4777db966aeaef780342151be653476b2bf72928
Evgeny Vetlugin <vetlugin@gmail.com>
2019-06-03 19:11:08 +0300

    Decomposed functions

--- reposts.py
+++ reposts.py
@@ -6,74 +6,84 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN=os.getenv("TOKEN")
+VK_TOKEN=os.getenv("VK_TOKEN")
+VK_VERSION=os.getenv("VK_VERSION")
+VK_GROUP_ID=os.getenv("VK_GROUP_ID")
 TELEGRAM_TOKEN=os.getenv("TELEGRAM_TOKEN")
 FACEBOOK_GROUP_ID=os.getenv("FACEBOOK_GROUP_ID")
 FACEBOOK_TOKEN=os.getenv("FACEBOOK_TOKEN")
-GROUP_ID=os.getenv("GROUP_ID")
-VK_VERSION=os.getenv("VK_VERSION")
 
 
-def vk_post():
+def url_check(path):
+    #return True if path[0:4]=='http://' else False
+    return True if path.find('://') >= 0 else False
+
+
+def vk_post(message, pic_path):
     try:
-        vk_session = vk_api.VkApi(token=TOKEN)
+        vk_session = vk_api.VkApi(token=VK_TOKEN)
     except vk_api.AuthError as error_msg:
         print(error_msg)
         return
 
     upload = vk_api.VkUpload(vk_session)
 
-    photo = upload.photo(
-        'fotoshar.jpeg',
-        album_id=263357401,
-        group_id=182385389
-    )
+    if not url_check(pic_path):
+        photo = upload.photo(
+            pic_path,
+            album_id=263357401,
+            group_id=VK_GROUP_ID
+        )
+        vk = vk_session.get_api()
+        vk.wall.post(
+            owner_id='{}'.format(photo[0]['owner_id']),
+            from_group=1,
+            message=message,
+            attachments='photo{}_{}'.format(photo[0]['owner_id'], photo[0]['id'])
+        )
+    else:
+        return
 
-    vk = vk_session.get_api()
 
-    vk.wall.post(
-        owner_id='{}'.format(photo[0]['owner_id']),
-        from_group=1,
-        message='Привет - это шарик',
-        attachments='photo{}_{}'.format(photo[0]['owner_id'], photo[0]['id'])
-    )
-
-
-def telegram_post():
+def telegram_post(message, pic_path):
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    print(bot.get_me())
-    bot.send_message(chat_id='@sphera_sveta', text="I'm sorry Dave I'm afraid I can't do that.")
-    bot.send_photo(chat_id='@sphera_sveta', photo='https://yadi.sk/i/_PE1oKs-kBmK0A')
+    bot.send_message(chat_id='@sphera_sveta', text=message)
+    if url_check(pic_path):
+        bot.send_photo(chat_id='@sphera_sveta', photo=pic_path)
+    else:
+        bot.send_photo(chat_id='@sphera_sveta', photo=open(pic_path, 'rb'))
 
 
-def facebook_post():
-    #context = f'https://graph.facebook.com/{FACEBOOK_GROUP_ID}/feed'
+def facebook_post(message, pic_path):
 
     context = f'https://graph.facebook.com/v3.3/{FACEBOOK_GROUP_ID}/photos'
 
-    open_file = open('kino.jpg', 'rb')
-    files = {'file': open_file}
-
     payload = {
-        #'url': 'http://www.spherasveta.ru/img/image/hanty_13.jpg',
-        'caption':'text and photo',
-#        'message': "АЛЛЕЛУЙАА!",
+        'caption':message,
         'access_token': FACEBOOK_TOKEN,
     }
-
-    response = requests.post(context, files=files, params=payload)
-    if not response.ok:
-        return
-    
-    #response = requests.post(context, params=payload).json()
     try:
-        print(response)
+        if not url_check(pic_path):
+            open_file = open(pic_path, 'rb')
+            files = {'file': open_file}
+            response = requests.post(context, files=files, params=payload)
+            if not response.ok:
+                return
+        else:
+            payload['url'] = pic_path
+            response = requests.post(context, params=payload)
+        return response
     except KeyError:
         return
 
 
 def main():
-    facebook_post()
+    message = 'Hello World'
+    pic_path = 'fotoshar.jpeg'
+
+    vk_post(message, pic_path)
+    telegram_post(message, pic_path)
+    facebook_post(message, pic_path)
 
 if __name__ == '__main__':
     main()
