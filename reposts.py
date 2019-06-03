46082959d842568490235362271a31ec71729047
Evgeny Vetlugin <vetlugin@gmail.com>
2019-06-03 09:48:16 +0300

    Add bot ability to post picture

--- reposts.py
+++ reposts.py
@@ -1,16 +1,17 @@
 import os
 import vk_api
+import telegram
 from dotenv import load_dotenv
 
 load_dotenv()
 
 TOKEN=os.getenv("TOKEN")
+TTOKEN=os.getenv("TTOKEN")
 GROUP_ID=os.getenv("GROUP_ID")
 VK_VERSION=os.getenv("VK_VERSION")
 
 
-def main():
-
+def vk_photo_post():
     try:
         vk_session = vk_api.VkApi(token=TOKEN)
     except vk_api.AuthError as error_msg:
@@ -33,6 +34,14 @@ def main():
         message='Привет - это шарик',
         attachments='photo{}_{}'.format(photo[0]['owner_id'], photo[0]['id'])
     )
+
+
+def main():
+    bot = telegram.Bot(token=TTOKEN)
+    print(bot.get_me())
+    bot.send_message(chat_id='@sphera_sveta', text="I'm sorry Dave I'm afraid I can't do that.")
+    bot.send_photo(chat_id='@sphera_sveta', photo='https://yadi.sk/i/_PE1oKs-kBmK0A')
+
 
 
 if __name__ == '__main__':
